56d5139f52337236492c68fd75893dfd8697e3c4
IlonaNovik <ilonan@zerobang.capital>
2024-09-08 08:17:17 +0200

    fix image

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import { ContactDetails } from "./components/contact-details";
 import { ContactForm } from "./components/contact-form";
 import ImgNeonCityMob from "../assets/images/neon-city-mob.webp";
@@ -9,26 +7,15 @@ export default function Contact() {
   return (
     <section id="contact">
       <div className="relative h-[40rem] overflow-hidden md:absolute md:left-0 md:h-full md:w-2/5 lg:w-1/2">
-        <Image
-          src={ImgNeonCity.src}
-          alt="Background image of a neon city"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          className="blur md:block"
-          loading="lazy"
-        />
-
-        <Image
-          src={ImgNeonCityMob.src}
-          alt="Background image of a neon city"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          className="blur md:hidden"
-          loading="lazy"
-        />
-
+        <picture>
+          <source srcSet={ImgNeonCity.src} media="(min-width: 768px)" />
+          <source srcSet={ImgNeonCityMob.src} media="(max-width: 767px)" />
+          <img
+            src={ImgNeonCity.src}
+            alt="Background image of a neon city"
+            className="size-full object-cover object-center blur"
+          />
+        </picture>
         <div className="absolute left-0 top-0 flex size-full items-center justify-center">
           <ContactDetails />
         </div>
