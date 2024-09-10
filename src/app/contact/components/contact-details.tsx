35b7691358054b37d7a093e5cc16e6665c057fe9
IlonaNovik <ilonan@zerobang.capital>
2024-09-10 18:22:11 +0200

    fix

--- src/app/contact/components/contact-details.tsx
+++ src/app/contact/components/contact-details.tsx
@@ -1,6 +1,6 @@
 import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/outline";
 
-import { ReactComponent as WhatsAppIcon } from "../../assets/icons/whatsapp.svg";
+import { WhatsAppIcon } from "@/app/components/icons";
 
 export const ContactDetails = () => {
   return (
